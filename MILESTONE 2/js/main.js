43ea410c797602023fcb12cd7b61de6fe9db9403
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-05-23 16:00:09 +0200

    CREATED MILESTONE 2

---
+++ MILESTONE 2/js/main.js
@@ -0,0 +1,31 @@
+//IMPORTS
+import contacts from "./data.js"
+
+var DateTime = luxon.DateTime;
+
+console.log(contacts)
+// VUE
+const { createApp } = Vue
+
+
+createApp({
+    data(){
+        return {
+            contacts,
+            currentChat: 0,
+        }
+    },
+    methods:{
+        getLastMessage(index){
+            return this.contacts[index].messages.at(-1);
+        },
+        getDateFromString(dateString){
+            const date = DateTime.fromFormat(dateString, "dd/MM/yyyy HH:mm:ss", {locale:"it"})
+            console.log(date)
+            return date;
+        },
+        dateToTime(date){
+            return date.toLocaleString(DateTime.TIME_24_SIMPLE)
+        },
+    }
+}).mount(".app")
