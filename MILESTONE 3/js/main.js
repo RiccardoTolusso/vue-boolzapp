df4738af1693a9ff39bcb89c414d9784e1c32ffe
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-05-23 17:06:39 +0200

    MILESTONE 3 COMPLETED: i had to fix the possible problme when the user swaps page while the timeout is not yet done.

--- MILESTONE 3/js/main.js
+++ MILESTONE 3/js/main.js
@@ -13,6 +13,7 @@ createApp({
         return {
             contacts,
             activeChat: 0,
+            inputTexts: []
         }
     },
     methods:{
@@ -21,7 +22,6 @@ createApp({
         },
         getDateFromString(dateString){
             const date = DateTime.fromFormat(dateString, "dd/MM/yyyy HH:mm:ss", {locale:"it"})
-            console.log(date)
             return date;
         },
         dateToTime(date){
@@ -32,6 +32,39 @@ createApp({
         },
         changeActive(newIndex){
             this.activeChat = newIndex;
-        }
+        },
+        addMessage(index, message, status){
+            // dato l'indice corrispondente al contatto e un messaggio aggiunge il messaggio alla lista dei messaggi
+            this.contacts = this.contacts.map((contact, i) => {
+                if (i !== index){
+                    // se il contatto non è quello da modificare
+                    return contact
+                }
+                else {
+                    // se è il contatto da modificare genero un nuovo contatto identico
+                    const newContact = {
+                        ...contact
+                    }
+                    newContact.messages.push(
+                        {
+                            date: DateTime.now().toFormat("dd/MM/yyyy HH:mm:ss"),
+                            message: message,
+                            status: status
+                        }
+                    )
+                    return newContact
+                }
+
+            })
+        },
+        sendMessage(){
+            this.addMessage(this.activeChat, this.inputTexts[this.activeChat], "sent")
+            this.inputTexts[this.activeChat] = null;
+            const activeChat = this.activeChat
+            setTimeout(()=>{
+                this.addMessage(activeChat, "ok", "received")
+            }, 1_000)
+            console.log("qua")
+        }   
     }
 }).mount(".app")
--- MILESTONE 3/readME.md
+++ MILESTONE 3/readME.md
@@ -1,3 +1,7 @@
-**Milestone 2**
-- Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
-- Click sul contatto mostra la conversazione del contatto cliccato
+**Milestone 3**
+- Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+
+modifico i miei contatti per avere un parametro textInput = null
+così al cambio della finestra la chat si resetta ma non perdo ciò che avevo inserito
+
+- Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
