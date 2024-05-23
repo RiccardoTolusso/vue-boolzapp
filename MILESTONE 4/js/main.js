0ed6d5da9753d1fb73720eabc9ddce65d0db7f45
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-05-23 17:08:12 +0200

    CREATED MILESTONE 4

---
+++ MILESTONE 4/js/main.js
@@ -0,0 +1,70 @@
+//IMPORTS
+import contacts from "./data.js"
+
+var DateTime = luxon.DateTime;
+
+console.log(contacts)
+// VUE
+const { createApp } = Vue
+
+
+createApp({
+    data(){
+        return {
+            contacts,
+            activeChat: 0,
+            inputTexts: []
+        }
+    },
+    methods:{
+        getLastMessage(index){
+            return this.contacts[index].messages.at(-1);
+        },
+        getDateFromString(dateString){
+            const date = DateTime.fromFormat(dateString, "dd/MM/yyyy HH:mm:ss", {locale:"it"})
+            return date;
+        },
+        dateToTime(date){
+            return date.toLocaleString(DateTime.TIME_24_SIMPLE)
+        },
+        getTimeFromLastMessage(index){
+            return this.dateToTime( this.getDateFromString( this.getLastMessage(index).date ) )
+        },
+        changeActive(newIndex){
+            this.activeChat = newIndex;
+        },
+        addMessage(index, message, status){
+            // dato l'indice corrispondente al contatto e un messaggio aggiunge il messaggio alla lista dei messaggi
+            this.contacts = this.contacts.map((contact, i) => {
+                if (i !== index){
+                    // se il contatto non è quello da modificare
+                    return contact
+                }
+                else {
+                    // se è il contatto da modificare genero un nuovo contatto identico
+                    const newContact = {
+                        ...contact
+                    }
+                    newContact.messages.push(
+                        {
+                            date: DateTime.now().toFormat("dd/MM/yyyy HH:mm:ss"),
+                            message: message,
+                            status: status
+                        }
+                    )
+                    return newContact
+                }
+
+            })
+        },
+        sendMessage(){
+            this.addMessage(this.activeChat, this.inputTexts[this.activeChat], "sent")
+            this.inputTexts[this.activeChat] = null;
+            const activeChat = this.activeChat
+            setTimeout(()=>{
+                this.addMessage(activeChat, "ok", "received")
+            }, 1_000)
+            console.log("qua")
+        }   
+    }
+}).mount(".app")
---
+++ MILESTONE 4/readME.md
@@ -0,0 +1,7 @@
+**Milestone 3**
+- Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+
+modifico i miei contatti per avere un parametro textInput = null
+così al cambio della finestra la chat si resetta ma non perdo ciò che avevo inserito
+
+- Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
